8666ce0d7e1d447657b7a91e83cee9dec4e49657
Tarun Ravada <tarunravada@gmail.com>
2022-07-05 17:19:15 +0530

    changed configs

--- scripts/train_data_230.sh
+++ scripts/train_data_230.sh
@@ -10,4 +10,8 @@ python train_net.py \
     --image_path_val        ../data/item_data_230 \
     --config-file           ../configs/item_data_230/fast_rcnn_R_50_FPN_3x.yaml \
     OUTPUT_DIR  ../outputs/item_data_230/fast_rcnn_R_50_FPN_3x/ \
-    SOLVER.IMS_PER_BATCH 2
+    SOLVER.IMS_PER_BATCH 2 \
+    SOLVER.BASE_LR 0.00025 \
+    SOLVER.CHECKPOINT_PERIOD 1000 \
+    DATALODER.NUM_WORKERS 2 \
+    MODEL.DEVICE cuda
